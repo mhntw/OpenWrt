227f4f9fa7460016282edc090c6dd99483f53a5d
mhntw <5mjss2tdtn@privaterelay.appleid.com>
2026-05-01 22:34:27 +0800

    feat: 设置默认 LAN IP 192.168.2.1、WiFi Huawei5G/abc123abc

--- scripts/init-settings.sh
+++ scripts/init-settings.sh
@@ -2,7 +2,20 @@
 
 # ============================================
 # JDCloud AX1800 Pro 固化配置
-# 恢复 OpenWrt 默认设置，无自定义密码/网络
+# LAN IP: 192.168.2.1
+# WiFi: Huawei5G / abc123abc
 # ============================================
+
+# 设置 LAN IP
+uci set network.lan.ipaddr='192.168.2.1'
+uci commit network
+
+# 设置 WiFi
+uci set wireless.@wifi-iface[0].ssid='Huawei5G'
+uci set wireless.@wifi-iface[0].key='abc123abc'
+uci set wireless.@wifi-iface[0].encryption='psk2'
+uci set wireless.@wifi-iface[0].disabled='0'
+uci set wireless.radio0.disabled='0'
+uci commit wireless
 
 exit 0
